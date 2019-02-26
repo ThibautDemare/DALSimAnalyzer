2a15b908e529f3d082766386fa3fd48c4ad3a6ee
ThibautDemare <fdhp_76@hotmail.com>
2019-02-26 15:42:14 +0100

    I should not create new process when I execute these shell commands

--- extract.py
+++ extract.py
@@ -4,12 +4,12 @@ import datetime
 import os.path
 
 if not os.path.isdir("Results"):
-	subprocess.Popen("mkdir Results")
+	subprocess.run("mkdir Results")
 extractionDate = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 if not os.path.isdir("Results/"+extractionDate):
-	subprocess.Popen("mkdir Results/"+extractionDate)
+	subprocess.run("mkdir Results/"+extractionDate)
 
 for i in range(1, 9):
 	pathCurrentRun = "Results/"+extractionDate+"/run"+str(i)
-	subprocess.Popen("mkdir "+pathCurrentRun)
+	subprocess.run("mkdir "+pathCurrentRun)
 	subprocess.Popen("scp -r "+username+"@"+servername+":~/simus022019/run"+str(i)+"/CSV "+pathCurrentRun)
