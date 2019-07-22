b8828b9aab7960b4a6b4a7164b28cae87ee4178b
ThibautDemare <fdhp_76@hotmail.com>
2019-07-22 14:37:33 +0200

    Make the script configurable: folder name and number of simulations passed through cli args

--- extract.py
+++ extract.py
@@ -2,14 +2,17 @@ from server_access_infos import *
 import subprocess
 import datetime
 import os.path
+import sys
 
 if not os.path.isdir("Results"):
 	subprocess.run("mkdir Results")
+
 extractionDate = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
 if not os.path.isdir("Results/"+extractionDate):
 	subprocess.run("mkdir Results/"+extractionDate)
 
-for i in range(1, 9):
+for i in range(1, int(sys.argv[2]) + 1):
 	pathCurrentRun = "Results/"+extractionDate+"/run"+str(i)
 	subprocess.run("mkdir "+pathCurrentRun)
-	subprocess.Popen("scp -r "+username+"@"+servername+":~/simus022019/run"+str(i)+"/CSV "+pathCurrentRun)
+	subprocess.Popen("scp -r "+username+"@"+servername+":"+str(sys.argv[1])+"/run"+str(i)+"/CSV "+pathCurrentRun)
