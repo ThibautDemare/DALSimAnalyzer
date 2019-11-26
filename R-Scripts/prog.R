6ceb6e73a2cc4ac528b96ae991141cba478d4c59
ThibautDemare <fdhp_76@hotmail.com>
2019-11-26 14:09:32 +0100

    Filter the most correlated (and useless) variables + add lines of the linear model on the charts

--- R-Scripts/prog.R
+++ R-Scripts/prog.R
@@ -2,7 +2,7 @@ source("fonctions.R")
 
 # chargement des données dans allData
 
-allData = csvIntoDF()
+allData = csvIntoDF("../averageResults-pandas/test/")
 
 # étape 1 : Suppression des colonnes à 0
 # on vire les var constantes sauf initialisation à 0
@@ -27,24 +27,44 @@ allData = allData[2000:16000,]
 lesCorrels = lesCorrelations(allData, 6000);
 
 for(i in 1:nrow(lesCorrels)) {
-	print(lesCorrels[i,]);
-	nomVar1 = lesCorrels[i,]$var1
-	nomVar2 = lesCorrels[i,]$var2
-	# device divisé en trois ligne
-	par(mfrow=c(3,1))
-	rangeVar1 = c(min(allData[[nomVar1]]),max(allData[[nomVar1]]))
-	rangeVar2 = c(min(allData[[nomVar2]]),max(allData[[nomVar2]]))
+	# ne pas afficher > 0.97
+	if(abs(lesCorrels[i,]$correl) < 0.97) {
+		print(lesCorrels[i,]);
+		nomVar1 = lesCorrels[i,]$var1
+		nomVar2 = lesCorrels[i,]$var2
+		# device divisé en trois ligne
+		par(mfrow=c(4,1))
+		rangeVar1 = c(min(allData[[nomVar1]]),max(allData[[nomVar1]]))
+		rangeVar2 = c(min(allData[[nomVar2]]),max(allData[[nomVar2]]))
 
-	# affichage de tout en 3 couleurs selon le pas de temps
-	plot(allData[[nomVar1]],allData[[nomVar2]], xlab=nomVar1, ylab=nomVar2, xlim=rangeVar1, ylim=rangeVar2, col = c(rep("red",6000),rep("black",2000),rep("green",6000)))
+		# affichage de tout en 3 couleurs selon le pas de temps
+		plot(allData[[nomVar1]],allData[[nomVar2]], xlab=nomVar1, ylab=nomVar2, xlim=rangeVar1, ylim=rangeVar2, col = c(rep("red",6000),rep("black",2000),rep("green",6000)))
+		reg = lm(allData[[nomVar2]] ~ allData[[nomVar1]])
+		print(reg)
+		abline(reg, col="blue")
 
-	# affichage de la première phase (avant ouverture canal)
-	plot(allData[[nomVar1]][1:6000],allData[[nomVar2]][1:6000], xlab=nomVar1, ylab=nomVar2, col="red", xlim=rangeVar1, ylim=rangeVar2)
+		# affichage de la première phase (avant ouverture canal)
+		plot(allData[[nomVar1]][1:6000],allData[[nomVar2]][1:6000], xlab=nomVar1, ylab=nomVar2, col="red", xlim=rangeVar1, ylim=rangeVar2)
+		reg = lm(allData[[nomVar2]][1:6000] ~ allData[[nomVar1]][1:6000])
+		print(reg)
+		abline(reg, col="blue")
 
-	# affichage de la 2e phase (après ouverture canal et stabilasation)
-	plot(allData[[nomVar1]][8000:14000],allData[[nomVar2]][8000:14000], xlab=nomVar1, ylab=nomVar2, col="green", xlim=rangeVar1, ylim=rangeVar2)
+		# affichage de la 2e phase (après ouverture canal et stabilasation)
+		plot(allData[[nomVar1]][8000:14000],allData[[nomVar2]][8000:14000], xlab=nomVar1, ylab=nomVar2, col="green", xlim=rangeVar1, ylim=rangeVar2)
+		reg = lm(allData[[nomVar2]][8000:14000] ~ allData[[nomVar1]][8000:14000])
+		print(reg)
+		abline(reg, col="blue")
 
-	readline();
+		corByTime =corByTimeStep(allData[[nomVar1]],allData[[nomVar2]], focusLength=2000, stepLength=250)
+		plot(corByTime, col="blue", xlab="step", ylab="Cor", pcl=23, ylim=c(-1,1))
+		# abline(v=length(corByTime)/2)
+		# 14000/250/2
+		# [1] 28
+		# > 16000/250/2
+		# [1] 32
+		abline(v=24) # Draw a vertical line to show the opening of the SCN
+
+		readline();
+	}
 }
 
-# multidraw par(new=TRUE)
